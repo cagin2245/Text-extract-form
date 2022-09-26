7ba8f798008d6117d7355cd4b22a0ff77db5fdb8
cagin <caginkilincyt@gmail.com>
2022-09-26 10:32:13 +0300

    comp

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import re
 import ast
 import itertools
 import numpy as np
-
+from pprint import pprint
 
 from difflib import SequenceMatcher
 
@@ -23,7 +23,7 @@ def is_string_similar(s1: str,s2: str, treshold: float = 0.50): # String karşı
 
 def textDetect(): #AWS Textract bağlantısı ve response döndürülmesi
     response = None
-    file = open("demo.jpeg","rb")
+    file = open("demo.jpg","rb")
     binaryFile = file.read()    
     client = boto3.client('textract',"us-east-1")
     response = client.analyze_expense(
@@ -43,24 +43,100 @@ if __name__ == "__main__":
     #     for line in input_file:
     #         strCompare.append(line)
     # response = textDetect()
-    # with open("AWS_AnalyzeExpense_RESPONSE.txt","w") as aws_response:
+    # with open("AWS_AnalyzeExpense_RESPONSE.txt","w",encoding="utf8") as aws_response:
     #     aws_response.write(str(response))
    
     
     
-    
-    
-    with open('AWS_AnalyzeExpense_RESPONSE.txt') as responseFile:
+    with open('AWS_AnalyzeExpense_RESPONSE.txt',encoding="utf8") as responseFile:
         data = responseFile.read()
         response = ast.literal_eval(data)
-    print(response)
     
     
+    def textractToArray(response):
+        #tables = [{"header": {"EXPENSE_ROW": 0} , "body" : {"EXPENSE_ROW" :0}}]*4
+        tables = [{"header":{"EXPENSE_ROW" : 0},"body": {}}] *2
+        forms = [{"Type": 0},{"Confidence": 0}]
+        i = 0
+        tNum = 0
+        array = response['ExpenseDocuments']
+        
+        for r in array:
+            
+            if(r['SummaryFields']):
+                for sf in r['SummaryFields']:
+                    
+                    
+                    for val in sf:
+                                        
+                        if val == "Type":
+                            forms[i]['Type'] = [{"Type": sf['Type']['Text']}, {"Confidence": sf['Type']['Confidence']}]
+                               
+                        elif val == "ValueDetection":
+                            
+                            forms[i]['Value'] = [{"value" : sf["ValueDetection"]['Text']}]
+                               
+                            
+                        elif val == "LabelDetection":
+                            forms[i]['label'] = [{"value" : sf['LabelDetection']['Text']}, {"Confidence" : sf['LabelDetection']['Confidence']}]
+                            
+                        
+            if(r['LineItemGroups']):
+                
+                for lig in r['LineItemGroups']:
+                    
+                    tableIndex = lig['LineItemGroupIndex']
+                    
+                    
+                    
+                    print(tNum)
+                    for table_r in lig['LineItems']:
+                        
+                        tableIndex = tNum
+                        
+                        for tr in table_r['LineItemExpenseFields']:
+                            tNum += 1
+                            print(tNum)
+                            if(tr['Type']['Text']=="EXPENSE_ROW"):
+                                
+                                tables[tableIndex]['header']['EXPENSE_ROW'] == "EXPENSE_ROW"
+                                
+                                
+                            for val in tr:
+                                if(tNum == 1): # Header
+                                    
+                                    if val == "LabelDetection":
+                                        sVal = tr['LabelDetection']['Text']
+                                        
+                                        sVal = re.sub('/\([^)]+\)/','',sVal)
+                                        print(sVal)
+                                        tables[tableIndex]['header'][tr['Type']['Text']] = sVal
+                                    
+                                        
+                                        
+                                        
+                                if val == 'ValueDetection':
+                                    
+                                    if(bool(tables[tableIndex]['header'][tr['Type']['Text']])):
+                                        print(tables[tableIndex]['header'][tr['Type']['Text']])
+                                        
+                                        tables[tableIndex]['body'][tr['Type']['Text']] = tr['ValueDetection']['Text']
+                            if(tr['Type']['Text']=="EXPENSE_ROW"):
+                                tables[tableIndex]['body'][tables[tableIndex]['header']['EXPENSE_ROW']] = tr['ValueDetection']['Text']
+            
+        retArr = [{"forms" : forms, "tables" : tables}]
+        return retArr
+                                    
+    arr = textractToArray(response)
+    pprint(arr)
+                       
+                                
+                            
+            
+                        
+                    
+                    
     
-    valSet = set()
-    text = []
-    l1 = []
-    tdata = []
     
                  
                                                 
@@ -117,13 +193,12 @@ if __name__ == "__main__":
     #             for strr in strCompare:
     #                 if(is_string_similar(textt,strr)):
     #                     text.append(item['Text']) 
-    blocks = response['Blocks']
-    strBlocks = json.dumps(blocks,indent=4)
-    with open("Output.txt","w") as text_file:
-        strtext = '\n'.join(text)
-        text_file.write(strtext)
-        #   with open("OutputJSON.txt","w")as json_file:   -> JSON output
-        #   json_file.write(strBlocks)    
+    
+    # with open("Output.txt","w") as text_file:
+    #     strtext = '\n'.join(text)
+    #     text_file.write(strtext)
+    with open("OutputJSON.txt","w")as json_file:   
+        json_file.write(str(arr))    
 
     
     
